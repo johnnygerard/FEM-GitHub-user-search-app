bff3a68e53c7eac589e10259aa7940ccc2865406
Johnny Gérard <mail@jgerard.dev>
2023-03-25 15:53:42 +0100

    feat: generate theme switch component

---
+++ src/app/theme-switch/theme-switch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-switch',
+  templateUrl: './theme-switch.component.html',
+  styleUrls: ['./theme-switch.component.scss']
+})
+export class ThemeSwitchComponent {
+
+}
