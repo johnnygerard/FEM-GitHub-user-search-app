6b2008cb030b4caa4f2cc005297c970ba41beaf9
Johnny Gérard <mail@jgerard.dev>
2023-03-25 15:48:00 +0100

    feat: theme service

--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ThemeService {
+  private dark = false;
 
-  constructor() { }
+  constructor() {}
+
+  toggleTheme() {
+    this.dark = !this.dark;
+    document.documentElement.classList.toggle("dark");
+  }
 }
