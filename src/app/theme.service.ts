e0687791df70428095231d99913ce0b178458cac
Johnny Gérard <mail@jgerard.dev>
2023-03-26 19:38:27 +0200

    feat: theme default and persistence (#8)

--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -4,14 +4,36 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class ThemeService {
-  private _isDark = false;
+  private _isDark: boolean;
+  private readonly THEME_KEY = "isDark";
+  private readonly THEME_CLASS = "dark";
 
-  get isDark() {
+  constructor() {
+    // Restore user theme from local storage if present otherwise use browser/system theme
+    this._isDark = localStorage[this.THEME_KEY]
+      ? JSON.parse(localStorage[this.THEME_KEY])
+      : matchMedia("(prefers-color-scheme: dark)").matches;
+
+    // Apply theme
+    if (this._isDark) document.documentElement.classList.add(this.THEME_CLASS);
+  }
+
+  get isDark(): boolean {
     return this._isDark;
   }
 
-  toggleTheme() {
+  // Toggle app theme between light and dark
+  toggleTheme(): void {
     this._isDark = !this._isDark;
-    document.documentElement.classList.toggle("dark");
+    document.documentElement.classList.toggle(this.THEME_CLASS);
+
+    // Persist user theme setting in localStorage
+    try {
+      localStorage[this.THEME_KEY] = this._isDark.toString();
+    } catch (e) {
+      if (e instanceof DOMException && e.name === "QuotaExceededError")
+        console.error(e);
+      else throw e;
+    }
   }
 }
