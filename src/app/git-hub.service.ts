353f76b210a1f3c6db01409b1ca14b42b62ea117
Johnny Gérard <mail@jgerard.dev>
2023-03-29 01:07:42 +0200

    feat: GitHub service GET request

--- src/app/git-hub.service.ts
+++ src/app/git-hub.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, retry, Subject, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GitHubService {
+  readonly userInfoSubject = new Subject<Object>();
+  private loading = false;
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
+
+  // Send GET request to fetch user info from GitHub REST API
+  getUserInfo(username: string): void {
+    // Prevent concurrent requests
+    if (this.loading) return;
+    this.loading = true;
+
+    this.client
+      .get(`https://api.github.com/users/${this.sanitize(username)}`, {
+        headers: {
+          Accept: 'application/vnd.github+json',
+        },
+      }).pipe(
+        // Only retry status 0 errors (network errors)
+        retry({
+          count: 3,
+          delay: (error: HttpErrorResponse, retryCount) => {
+            if (error.status) return throwError(() => error); // Forward HTTP errors
+            return new Observable(subscriber => {
+              // Retry request after increasing delay (10, 100, 1000 ms)
+              setTimeout(() => subscriber.next(true), 10 ** retryCount);
+            });
+          },
+        })
+      ).subscribe({
+        complete: () => this.loading = false,
+        next: json => this.userInfoSubject.next(json),
+        error: (error: HttpErrorResponse) => {
+          this.loading = false;
+          // Log error and notify user
+          console.error(error);
+          alert('Failed to retrieve user info. Please try again later.');
+        },
+      });
+  }
+
+  private sanitize(username: string): string {
+    return username ? encodeURIComponent(username) : 'octocat';
+  }
 }
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GitHubService } from '../git-hub.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
+  constructor(private github: GitHubService) { }
 
+  getUserInfo(username: string): void {
+    this.github.getUserInfo(username);
+  }
 }
