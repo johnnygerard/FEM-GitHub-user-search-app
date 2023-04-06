af451a39ed6374c8b2df2da5ed96982f9bbbef22
Johnny Gérard <mail@jgerard.dev>
2023-04-06 20:01:57 +0200

    feat: attribution

--- src/app/attribution/attribution.component.ts
+++ src/app/attribution/attribution.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-attribution',
   templateUrl: './attribution.component.html',
   styleUrls: ['./attribution.component.scss']
 })
-export class AttributionComponent {
+export class AttributionComponent implements AfterViewInit {
+  protected hidden = true;
 
+  @ViewChild('dialog') dialog!: ElementRef<HTMLDialogElement>;
+
+  ngAfterViewInit(): void {
+    this.dialog.nativeElement.close();
+    setTimeout(() => {
+      this.hidden = false;
+      this.dialog.nativeElement.showModal();
+    }, 500);
+  }
+
+  protected onClick(event: MouseEvent): void {
+    if (event.target === event.currentTarget)
+      this.dialog.nativeElement.close();
+  }
 }
